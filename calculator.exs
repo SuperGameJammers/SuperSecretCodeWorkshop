475c225a6e438d5959c4e1b03ede1efd8402ef87
Zura Guerra <guerrazuramx@gmail.com>
2016-03-31 19:06:56 -0600

    Pseudo calculator exercise

---
+++ calculator.exs
@@ -0,0 +1,9 @@
+defmodule Calculator do
+  def sum(first, second) do
+    first + second
+  end
+  
+  def substract(first, second) do
+    first - second
+  end
+end
--- greetings.exs
+++ greetings.exs
@@ -2,4 +2,8 @@ defmodule Greetings do
   def say_hi(who) do
     IO.puts("Hi, #{who}")
   end
+
+  def say_hi do
+    IO.puts("Hi, someone")
+  end
 end
